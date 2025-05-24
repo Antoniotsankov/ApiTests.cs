cc203a4a9bc6bd3e1480853ae3e429f89b463f5c
Antoniotsankov <antoniontsankov@gmail.com>
2025-05-24 15:50:33 +0300

    Add project files.

---
+++ ApiTests.cs/UnitTest1.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace ApiTestingDemo
+{
+    [TestFixture]
+    public class ApiTests
+    {
+        private HttpClient client;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            client = new HttpClient();
+        }
+
+        [OneTimeTearDown]
+        public void Cleanup()
+        {
+            client.Dispose(); // Това премахва warning-а
+        }
+
+        [Test]
+        public async Task SuccessfulLogin_ReturnsToken()
+        {
+            var url = "https://reqres.in/api/login";
+            var data = new StringContent(
+                "{\"email\":\"eve.holt@reqres.in\", \"password\":\"cityslicka\"}",
+                Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, data);
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(200, (int)response.StatusCode);
+            Assert.IsTrue(JObject.Parse(content).ContainsKey("token"));
+        }
+
+        [Test]
+        public async Task MissingPassword_ReturnsError()
+        {
+            var url = "https://reqres.in/api/login";
+            var data = new StringContent(
+                "{\"email\":\"eve.holt@reqres.in\"}",
+                Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync(url, data);
+            var content = await response.Content.ReadAsStringAsync();
+
+            Assert.AreEqual(400, (int)response.StatusCode);
+            Assert.IsTrue(content.Contains("Missing password"));
+        }
+    }
+}
